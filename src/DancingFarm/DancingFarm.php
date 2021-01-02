fcd949a2e5d03ec3c52a6384fc5fd78e555ec8d9
DontTouchMeXD <76811635+DontTouchMeXD@users.noreply.github.com>
2021-01-02 09:11:10 +0700

    Update DancingFarm.php

--- src/DancingFarm/DancingFarm.php
+++ src/DancingFarm/DancingFarm.php
@@ -6,8 +6,27 @@ use pocketmine\plugin\PluginBase;
 
 class DancingFarm extends PluginBase {
     
+    public const CONFIG_VERSION = 1;
+    
     public function onEnable() : void {
-        $this->saveResource("config.yml");
+        $this->saveDefaultConfig();
+        $this->checkConfig();
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    }
+    
+    private function checkConfig(): void {
+        if($this->getConfig()->get("config_version") < self::CONFIG_VERSION){
+            $this->getLogger()->notice("your config file is outdate.., creating a new config file");
+            $this->replaceConfig();
+        } 
+        if($this->getConfig()->get("config_version") > self::CONFIG_VERSION){
+            $this->getLogger()->notice("your config version is higher than the latest version..."); 
+            $this->replaceConfig(); 
+        }
+    }
+    
+    private function replaceConfig(): void {
+        rename($this->getDataFolder() . "config.yml", $this->getDataFolder() . "config_old.yml");
+        $this->saveResource("config.yml");  
     }
 }
